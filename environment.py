c6d6a510c7b6f1aed585deb6386bb015f1326858
Roberto David Morales Ramos <davld7@outlook.com>
2023-11-12 23:52:49 -0600

    validation error

--- environment.py
+++ environment.py
@@ -1,10 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import ValidationError
 from functools import lru_cache
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8")
+
+    def __init__(self):
+        try:
+            super().__init__()
+        except ValidationError as exception:
+            print(
+                f"Error loading environment variables from .env file:\n{str(exception)}")
+            exit(1)
 
 
 class Variables(Settings):
